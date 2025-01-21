af0c7657714303cadf5463f98073fc406c47b6ad
gitYoungren <youngrengitonga@gmail.com>
2025-01-21 10:22:29 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -14,13 +12,50 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    # Query the Earthquake with the given ID
+    earthquake = Earthquake.query.get(id)
+    
+    if earthquake:
+        # If found, return earthquake attributes as JSON
+        return jsonify({
+            'id': earthquake.id,
+            'magnitude': earthquake.magnitude,
+            'location': earthquake.location,
+            'year': earthquake.year
+        })
+    else:
+        # If not found, return error message as JSON with the 'message' key
+        return jsonify({'message': f'Earthquake {id} not found.'}), 404
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    # Query for all earthquakes with magnitude >= the given value
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    # Prepare the response data
+    earthquakes_data = [
+        {
+            'id': eq.id,
+            'location': eq.location,
+            'magnitude': eq.magnitude,
+            'year': eq.year
+        }
+        for eq in earthquakes
+    ]
+    
+    # Return the count and list of earthquakes
+    return jsonify({
+        'count': len(earthquakes_data),
+        'quakes': earthquakes_data
+    })
+
 
 
 if __name__ == '__main__':
